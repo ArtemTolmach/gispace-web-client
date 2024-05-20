595e509cc87ed5f62fae5e88f00295c7f2e177e9
Artem Tolmachev <artem.tolmachev.work@gmail.com>
2024-05-21 00:32:39 +0300

    Change craco to vite

---
+++ vite.config.js
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+      '@Components': resolve(__dirname, 'src/components'),
+      '@Assets': resolve(__dirname, 'src/assets'),
+      '@Pages': resolve(__dirname, 'src/pages'),
+      '@Router': resolve(__dirname, 'src/router'),
+      '@So_on': resolve(__dirname, 'src/so_on')
+    }
+  },
+  server: {
+    host: 'localhost',
+    port: 3000,
+  },
+  css: {
+    devSourcemap: true,
+    modules: {
+      scopeBehaviour: 'local'
+    }
+  }
+});
